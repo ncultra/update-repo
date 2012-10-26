6a1b62d1918d420b779dab9e590d1abcbe9d7ad8
Mike Day <ultra@ncultra.org>
2012-10-26 10:46:15 -0600

    added setup.py to install the program and modules

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+'''
+setup.py [--bin <binary dir>] [--mod <module dir>] 
+
+default /usr/local/bin, /usr/local/lib
+
+'''
+
+import os, sys, shutil, stat
+from optparse import OptionParser
+
+binpath = "/usr/local/bin/"
+modpath = "/usr/local/lib/"
+
+
+usage_string = "usage: %prog [--bin <binary location>] [--mod <module location>]"
+parser = OptionParser(usage=usage_string)
+parser.add_option("-b", "--binary", help="location for binary",
+                  default="/usr/local/bin/")
+parser.add_option("-m", "--mod", help="location for repo modules", 
+                  default="/usr/local/lib/")
+
+(options, args) = parser.parse_args()
+
+if options.binary:
+    binpath = options.binary + "/"
+if options.mod:
+    modpath = options.mod + "/"
+
+shutil.copy("update-repo.py", binpath)
+os.chmod(binpath + "update-repo.py", 755)
+shutil.copy("kvm_repository.py", modpath)
+shutil.copy("ovirt_repository.py", modpath)
+shutil.copy("openstack_repository.py", modpath)
+
+
+
