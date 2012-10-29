e307a81a822248c83ef13ca3949336ef047002bd
Mike Day <ultra@ncultra.org>
2012-10-29 18:57:03 -0400

    location of openstack repos has changed @ github

--- openstack_repository.py
+++ openstack_repository.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python
 
 SRC_PREFIX="/Users/mdday/src/openstack/"
-
+https://github.com/openstack/cinder.git
 repos = [(SRC_PREFIX + "ceilometer", "https://github.com/stackforge/ceilometer.git"),
-         (SRC_PREFIX + "cinder", "https://github.com/stackforge/cinder.git"),
-         (SRC_PREFIX + "glance", "https://github.com/stackforge/glance.git"), 
-         (SRC_PREFIX + "horizon", "https://github.com/stackforge/horizon.git"),
-         (SRC_PREFIX + "identity-api", "https://github.com/stackforge/identity-api.git"),
-         (SRC_PREFIX + "image-api", "https://github.com/stackforge/image-api.git"),
-         (SRC_PREFIX + "keystone", "https://github.com/stackforge/keystone.git"),
-         (SRC_PREFIX + "netconn-api", "https://github.com/stackforge/netconn-api.git"),
-         (SRC_PREFIX + "nova", "https://github.com/stackforge/nova.git"),
-         (SRC_PREFIX + "object-api", "https://github.com/stackforge/object-api.git"),
-         (SRC_PREFIX + "openstack-ci", "https://github.com/stackforge/openstack-ci.git"),
-         (SRC_PREFIX + "openstack-ci-puppet", "https://github.com/stackforge/openstack-ci-puppet.git"),
-         (SRC_PREFIX + "openstack-common", "https://github.com/stackforge/openstack-common.git"),
-         (SRC_PREFIX + "openstack-manuals", "https://github.com/stackforge/openstack-manuals.git"),
-         (SRC_PREFIX + "python-cinderclient", "https://github.com/stackforge/python-cinderclient.git"),
-         (SRC_PREFIX + "python-glanceclient", "https://github.com/stackforge/python-glanceclient.git"),
-         (SRC_PREFIX + "python-keystoneclient", "https://github.com/stackforge/python-keystoneclient.git"),
-         (SRC_PREFIX + "python-novaclient", "https://github.com/stackforge/python-novaclient.git"),
-         (SRC_PREFIX + "python-openstackclient", "https://github.com/stackforge/python-openstackclient.git"),
-         (SRC_PREFIX + "python-quantumclient", "https://github.com/stackforge/python-quantumclient.git"),
-         (SRC_PREFIX + "python-swiftclient", "https://github.com/stackforge/python-swiftclient.git"),
-         (SRC_PREFIX + "quantum", "https://github.com/stackforge/quantum.git"),
-         (SRC_PREFIX + "swift", "https://github.com/stackforge/swift.git"),
-         (SRC_PREFIX + "tempest", "https://github.com/stackforge/tempest.git")]
+         (SRC_PREFIX + "cinder", "https://github.com/cinder.git"),
+         (SRC_PREFIX + "glance", "https://github.com/glance.git"), 
+         (SRC_PREFIX + "horizon", "https://github.com/horizon.git"),
+         (SRC_PREFIX + "identity-api", "https://github.com/identity-api.git"),
+         (SRC_PREFIX + "image-api", "https://github.com/image-api.git"),
+         (SRC_PREFIX + "keystone", "https://github.com/keystone.git"),
+         (SRC_PREFIX + "netconn-api", "https://github.com/netconn-api.git"),
+         (SRC_PREFIX + "nova", "https://github.com/nova.git"),
+         (SRC_PREFIX + "object-api", "https://github.com/object-api.git"),
+         (SRC_PREFIX + "openstack-ci", "https://github.com/openstack-ci.git"),
+         (SRC_PREFIX + "openstack-ci-puppet", "https://github.com/openstack-ci-puppet.git"),
+         (SRC_PREFIX + "openstack-common", "https://github.com/openstack-common.git"),
+         (SRC_PREFIX + "openstack-manuals", "https://github.com/openstack-manuals.git"),
+         (SRC_PREFIX + "python-cinderclient", "https://github.com/python-cinderclient.git"),
+         (SRC_PREFIX + "python-glanceclient", "https://github.com/python-glanceclient.git"),
+         (SRC_PREFIX + "python-keystoneclient", "https://github.com/python-keystoneclient.git"),
+         (SRC_PREFIX + "python-novaclient", "https://github.com/python-novaclient.git"),
+         (SRC_PREFIX + "python-openstackclient", "https://github.com/python-openstackclient.git"),
+         (SRC_PREFIX + "python-quantumclient", "https://github.com/python-quantumclient.git"),
+         (SRC_PREFIX + "python-swiftclient", "https://github.com/python-swiftclient.git"),
+         (SRC_PREFIX + "quantum", "https://github.com/quantum.git"),
+         (SRC_PREFIX + "swift", "https://github.com/swift.git"),
+         (SRC_PREFIX + "tempest", "https://github.com/tempest.git")]
 
 
 
