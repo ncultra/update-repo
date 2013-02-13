221fb5642ae25f996c6fbba6d656d9f75338ae68
Mike Day <ultra@ncultra.org>
2013-02-13 07:53:39 -0500

    added support for the open efs repository

--- update-repo.sh
+++ update-repo.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository gnulib_repository"
+REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository gnulib_repository open_efs_repository"
 
 for repo in $REPOS; do
     update-repo.py $repo
