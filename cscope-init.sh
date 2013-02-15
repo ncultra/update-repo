16f05bc4ecf63b1004a8d0927aec43deba8fa9c2
Mike Day <ncultra@gmail.com>
2013-02-15 13:17:05 -0500

    added cscope init script

---
+++ cscope-init.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+#this is broken right now, trying to be too fancy
+#http://cscope.sourceforge.net/large_projects.html
+
+if [ $# -lt 1 ]
+then  
+    echo "usage: cscope-init <source dir>"
+    exit 1
+fi
+
+if [ ! -e $1 ] ; then
+    echo "$1 doesn't exist: enter an existing source directory."
+    exit 1
+fi
+
+pushd $1 &>/dev/null
+find ./ -name "*.c" -o -name "*.h" -o -name "*.py" -o \
+    -name "*.sh" -o -name "*.in" -o -name "*.am" -o \
+    -name "*.l" -o -name "*.y" -o name "*.java" > cscope.files
+ 
+cscope -q -R -b -i cscope.files
+
+echo "new cscope database is located in $1/cscope.out"
+ 
+popd &>/dev/null
