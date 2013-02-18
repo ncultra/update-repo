544479574dd2e4eb3b2050a857703e3187573f14
Mike Day <ultra@ncultra.org>
2013-02-18 14:59:00 -0500

    removed ovirt-guest-sdk repository

--- ovirt_repository.py
+++ ovirt_repository.py
@@ -5,7 +5,6 @@ SRC_PREFIX="/Users/mdday/src/ovirt/"
 repos = [(SRC_PREFIX + "ovirt-dwh", "git://gerrit.ovirt.org/ovirt-dwh"),
          (SRC_PREFIX + "ovirt-engine", "git://gerrit.ovirt.org/ovirt-engine"),
          (SRC_PREFIX + "ovirt-engine-sdk", "git://gerrit.ovirt.org/ovirt-engine-sdk"),
-         (SRC_PREFIX + "ovirt-guest-sdk", "git://gerrit.ovirt.org/ovirt-guest-sdk"),
          (SRC_PREFIX + "ovirt-guest-agent", "git://gerrit.ovirt.org/ovirt-guest-agent"),
          (SRC_PREFIX + "ovirt-node", "git://gerrit.ovirt.org/ovirt-node"),
          (SRC_PREFIX + "ovirt-reports", "git://gerrit.ovirt.org/ovirt-reports"),
