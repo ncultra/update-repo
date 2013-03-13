f5ee4926c4e54ad438d691ec07c5c4454443c499
Mike Day <ultra@ncultra.org>
2013-03-13 08:17:59 -0400

    added ctag and cscope options to shell file

--- update-repo.sh
+++ update-repo.sh
@@ -3,5 +3,5 @@
 REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository gnulib_repository open_efs_repository"
 
 for repo in $REPOS; do
-    update-repo.py $repo
+    update-repo.py -st $repo
 done
