9fb7345851ba0e949d03cf3efefa7257eee513f6
Mike Day <ncultra@gmail.com>
2013-04-02 14:39:48 -0400

    added glusterfs repository from github mirror

--- update-repo.sh
+++ update-repo.sh
@@ -1,7 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository gnulib_repository open_efs_repository"
 
 for repo in $REPOS; do
-    update-repo.py -st $repo
+    echo "updating $repo"
+   update-repo.py -st  $repo
 done
