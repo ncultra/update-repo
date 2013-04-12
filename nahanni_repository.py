294302949c44b832f137777c0083fe35bb206bc3
Mike Day <ncultra@gmail.com>
2013-04-12 11:53:09 -0400

    added nahanni repo

---
+++ nahanni_repository.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+SRC_PREFIX="/Users/mdday/src/"
+
+repos = [(SRC_PREFIX + "nahanni", "git://gitorious.org/nahanni/guest-code.git")]
--- update-repo.sh
+++ update-repo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository gnulib_repository open_efs_repository"
+REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository \
+       gnulib_repository open_efs_repository glusterfs_repository"
 
 for repo in $REPOS; do
     echo "updating $repo"
-   update-repo.py -st  $repo
+   update-repo.py "$@" $repo
 done
