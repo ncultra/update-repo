3b5c20ec182488bf6a4394d70d3a9f2be6207442
Mike Day <ncultra@gmail.com>
2013-05-06 12:08:03 -0400

    added new qemu repos for rdma, 390, ppc, and vfio. new kvm repo for vfio.

--- qemu-rdma_repository.py
+++ qemu-rdma_repository.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+## specialized Qemu repositories
+
 SRC_PREFIX="/Users/mdday/src/"
 
-repos = [(SRC_PREFIX + "qemu-rdma", "git://github.com/hinesmr/qemu.git", "rdma")]
+repos = [(SRC_PREFIX + "qemu-rdma", "git://github.com/hinesmr/qemu.git", "rdma"),
+         (SRC_PREFIX + "qemu-agraf" "git://github.com/agraf/qemu.git", "s390-for-upstream"),
+         (SRC_PREFIX + "qemu-vfio" "git://github.com/awilliam/qemu-vfio.git", "vfio-for-qemu")]
+# agraf also has a ppc-for-upstream branch
--- update-repo.sh
+++ update-repo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository \
-       gnulib_repository open_efs_repository glusterfs_repository"
+       gnulib_repository open_efs_repository glusterfs_repository qemu-rdma_repository \
+       nahanni_repository"
 
 for repo in $REPOS; do
     echo "updating $repo"
