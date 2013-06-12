000220f8c70f4307e020f5ead9cc661de5fb014a
Mike Day <ncultra@gmail.com>
2013-06-12 17:01:23 -0400

    added spice git to collection of qemu repositories.

---
+++ qemu_repository.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+## Qemu repositories
+
+SRC_PREFIX="/Users/mdday/src/"
+
+repos = [(SRC_PREFIX + "qemu/", "git://git.qemu.org/qemu.git"),
+         (SRC_PREFIX + "qemu-kvm/", "git://git.kernel.org/pub/scm/virt/kvm/qemu-kvm.git"),
+         (SRC_PREFIX + "qemu-rdma/", "git://github.com/hinesmr/qemu.git", "rdma"),
+         (SRC_PREFIX + "qemu-agraf/", "git://github.com/agraf/qemu.git", "s390-for-upstream"),
+         (SRC_PREFIX + "qemu-agraf/", "git://github.com/agraf/qemu.git", "ppc-for-upstream"),
+         (SRC_PREFIX + "qemu-vfio/", "git://github.com/awilliam/qemu-vfio.git", "vfio-for-qemu"),
+         (SRC_PREFIX + "qemu-aliguori/", "git://github.com/aliguori/qemu.git"),
+         (SRC_PREFIX + "spice-protocol", "git://anongit.freedesktop.org/spice/spice-protocol") ]
+# agraf also has a ppc-for-upstream branch
