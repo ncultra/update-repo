eaa15748cd72b6a91611a2fe35c7d81b5c3f28c9
Mike Day <ncultra@gmail.com>
2013-06-14 15:30:25 -0400

    added new xen repo module to shell script

--- update-repo.sh
+++ update-repo.sh
@@ -2,7 +2,7 @@
 
 REPOS="kvm_repository ovirt_repository openstack_repository libvirt_repository \
        gnulib_repository open_efs_repository glusterfs_repository qemu-rdma_repository \
-       nahanni_repository"
+       nahanni_repository xen_repository"
 
 for repo in $REPOS; do
     echo "updating $repo"
