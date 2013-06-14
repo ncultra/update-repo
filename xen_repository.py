3e75a08bf1574974d4cff97a5c1d8e01b8725538
Mike Day <ncultra@gmail.com>
2013-06-14 15:16:13 -0400

    added a couple of xen repositories, and added them to the setup script.

---
+++ xen_repository.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+## xen repositories
+
+SRC_PREFIX="/Users/mdday/src/"
+
+repos = [(SRC_PREFIX + "xen/", "git://xenbits.xen.org/xen.git"),
+         (SRC_PREFIX + "xen-testtools", "git://xenbits.xen.org/xentesttools/bootstrap.git"),
+         (SRC_PREFIX + "xen-unstable", "git://xenbits.xen.org/people/iwj/xen-unstable.git")]
