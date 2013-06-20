7e0ce1189b6f8203d2e79ba5be277e4168aabdfb
Mike Day <ncultra@gmail.com>
2013-06-20 12:23:39 -0400

    tweaked branch for kvm repo

--- kvm_repository.py
+++ kvm_repository.py
@@ -3,9 +3,9 @@
 
 SRC_PREFIX="/Users/mdday/src/"
 
-repos = [(SRC_PREFIX + "kvm-bonzini", "git://git.kernel.org/pub/scm/linux/kernel/git/bonzini/linux.git", 
-          {"cscope":"no", "tag": "no"}),
-         (SRC_PREFIX + "kvm-group", "git://git.kernel.org/pub/scm/virt/kvm/kvm.git"), 
+repos = [(SRC_PREFIX + "kvm-bonzini", "git://git.kernel.org/pub/scm/linux/kernel/git/bonzini/linux.git"),
+         (SRC_PREFIX + "kvm-group", "git://git.kernel.org/pub/scm/virt/kvm/kvm.git",  
+          {"branch":"next", "cscope":"no", "tag": "no"}), 
          (SRC_PREFIX + "kvm-unittests", "git://git.kernel.org/pub/scm/virt/kvm/kvm-unit-tests.git"),
          (SRC_PREFIX + "linux", "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"),
          (SRC_PREFIX + "linux-vfio/", "git://github.com/awilliam/linux-vfio.git", 
