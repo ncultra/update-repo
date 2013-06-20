27f7b551de1d3581c71ee4ae3a84db7110fb3a34
Mike Day <ncultra@gmail.com>
2013-06-20 08:22:46 -0400

    updated the kvm repository to reflect changes in maintainership

--- kvm_repository.py
+++ kvm_repository.py
@@ -3,8 +3,10 @@
 
 SRC_PREFIX="/Users/mdday/src/"
 
-repos = [(SRC_PREFIX + "kvm/", "git://git.kernel.org/pub/scm/virt/kvm/kvm.git", 
-          {"branch":"linux-next", "cscope":"no", "tag": "no"}), 
+repos = [(SRC_PREFIX + "kvm-bonzini", "git://git.kernel.org/pub/scm/linux/kernel/git/bonzini/linux.git", 
+          {"cscope":"no", "tag": "no"}),
+         (SRC_PREFIX + "kvm-group", "git://git.kernel.org/pub/scm/virt/kvm/kvm.git"), 
+         (SRC_PREFIX + "kvm-unittests", "git://git.kernel.org/pub/scm/virt/kvm/kvm-unit-tests.git"),
          (SRC_PREFIX + "linux", "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"),
          (SRC_PREFIX + "linux-vfio/", "git://github.com/awilliam/linux-vfio.git", 
           {"branch": "vfio-vga-reset"}),
