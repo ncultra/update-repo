89987d0c90d8c938cdae64521b336d1e0b2338fe
Mike Day <ncultra@gmail.com>
2013-07-03 17:33:10 -0400

    changed tracking to test-branch

--- update-repo_repository.py
+++ update-repo_repository.py
@@ -4,4 +4,4 @@
 SRC_PREFIX="/Users/mdday/src/"
 
 repos = [(SRC_PREFIX + "update-repo-test", "git@github.com:ncultra/update-repo.git",
-          {"branch":"re-branch", "cscope":"no", "tag": "no"})]
+          {"branch":"test-branch", "cscope":"no", "tag": "no"})]
