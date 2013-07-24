d9962e25be83f01bf5d4fe77eca5b4260bc82198
Mike Day <ncultra@gmail.com>
2013-07-24 08:52:36 -0400

    added bonzini qemu and removed spice repos to rdma module. Preparing to rename that module

--- qemu-rdma_repository.py
+++ qemu-rdma_repository.py
@@ -4,8 +4,7 @@
 
 SRC_PREFIX="/Users/mdday/src/"
 
-repos = [(SRC_PREFIX + "qemu-rdma/", "git://github.com/hinesmr/qemu.git", "rdma"),
-         (SRC_PREFIX + "qemu-agraf/", "git://github.com/agraf/qemu.git", "s390-for-upstream"),
-         (SRC_PREFIX + "qemu-agraf/", "git://github.com/agraf/qemu.git", "ppc-for-upstream"),
-         (SRC_PREFIX + "qemu-vfio/", "git://github.com/awilliam/qemu-vfio.git", "vfio-for-qemu")]
-# agraf also has a ppc-for-upstream branch
+repos = [(SRC_PREFIX + "spice-spice", "git://anongit.freedesktop.org/spice/spice"),
+         (SRC_PREFIX + "spice-qemu", "git://anongit.freedesktop.org/spice/qemu"),
+         (SRC_PREFIX + "spice-common", "git://anongit.freedesktop.org/spice/spice-common"),
+         (SRC_PREFIX + "spice-protocol", "git://anongit.freedesktop.org/spice/spice-protocol")]
